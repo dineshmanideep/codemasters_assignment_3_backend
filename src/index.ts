7642f39f6f9cf17aa29d21ebde3bd1353c81eda6
dineshmanideep <dineshmanideep@gmail.com>
2025-01-01 23:14:40 +0530

    final

--- src/index.ts
+++ src/index.ts
@@ -19,7 +19,7 @@ app.use(express.json())
 
 app.use(
     cors({
-      origin:"http://localhost:5173",
+      origin:"https://codemasters-assignment-3-frontend.vercel.app/",
       credentials: true, 
     })
   );
